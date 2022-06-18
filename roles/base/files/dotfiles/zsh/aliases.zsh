2e371aa28f32e54f3cb50156df1f840be537c809
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-06-18 01:49:44 +0100

    fix zsh logo

--- roles/base/files/dotfiles/zsh/aliases.zsh
+++ roles/base/files/dotfiles/zsh/aliases.zsh
@@ -2,7 +2,7 @@
 alias cls="clear"
 
 # Make neofetch use the custom logo
-alias neofetch="neofetch --source .config/neofetch/logo.txt --ascii_colors 213"
+alias neofetch="neofetch --source ~/.config/neofetch/logo.txt --ascii_colors 213"
 
 # Add sync command
 alias sync="ansible-pull -U https://github.com/BitsOfAByte/ansible-deploy.git --ask-become-pass"
